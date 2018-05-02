fd107bc7fdef033122c4fbba16790e95fa2f33ac
mohit-gh <mohitjajani85@gmail.com>
2018-05-02 16:50:27 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+$arr = ['mohit','sandeep','kd','ilyaqat','manoj'];
+
+foreach ($arr as $jymmemb) {
+	echo $jymmemb;
+	echo '<br/>';
+}
+Output:
+        mohit
+        sandeep
+        kd
+        ilyaqat
+        manoj
+        
+-----------------------------------------------------------------------------------------------------------------------------------
